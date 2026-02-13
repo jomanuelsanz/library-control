330354d34d47a90e63ea7d0b0be90e43441b7114
jomanuelsanz <jomanuelsanz@gmail.com>
2026-02-13 14:07:18 +0100

    Update App.java

--- src/main/java/pio/daw/App.java
+++ src/main/java/pio/daw/App.java
@@ -1,8 +1,11 @@
 package pio.daw;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class App {
+
     /**
      * Parse the arguments of the program to get the library registry file
      * path. Exits the program if the args are not correct or the file does
@@ -11,8 +14,21 @@ public class App {
      * @return Path to file if exists.
      */
     public static Path getPathFromArgs(String[] args){
-        //TODO
-        return null;
+        // Si no hay un argumento, salimos
+        if(args.length != 1){
+            System.err.println("Uso: NombrePrograma <Ruta fichero>");
+            System.exit(1);
+        }
+
+        Path path = Paths.get(args[0]);
+
+        // Si el archivo no existe, salimos
+        if(!Files.exists(path)){
+            System.err.println("El fichero no existe: " + args[0]);
+            System.exit(1);
+        }
+
+        return path;
     }
 
     public static void main(String[] args) {
