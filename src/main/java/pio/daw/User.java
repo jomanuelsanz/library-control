974add530fc92296737de0fea0fd3be6a0943731
jomanuelsanz <jomanuelsanz@gmail.com>
2026-02-13 14:09:01 +0100

    Update User.java

--- src/main/java/pio/daw/User.java
+++ src/main/java/pio/daw/User.java
@@ -1,9 +1,11 @@
 package pio.daw;
 
 public class User implements Localizable {
+
     private String id;
-    private EventType lasEvent = null;
+    private EventType lastEvent = null;
     private Boolean inside = false;
+    private int entryCount = 0;
 
     public User(String id){
         this.id = id;
@@ -13,8 +15,26 @@ public class User implements Localizable {
         return this.id;
     }
 
-    //TODO
+    // Contador de entradas
+    public int getEntryCount(){
+        return this.entryCount;
+    }
+
+    // El usuario entra
+    public void enter(){
+        this.inside = true;
+        this.lastEvent = EventType.ENTRY;
+        this.entryCount++;
+    }
+
+    // El usuario sale
+    public void exit(){
+        this.inside = false;
+        this.lastEvent = EventType.EXIT;
+    }
+
+    // Devuelve si está dentro
     public Boolean isInside(){
-        return null
+        return this.inside;
     }
 }
